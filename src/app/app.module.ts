03b2d32ce64e6b152249ec97b2bb37350b55f8d0
Feras Deiratany <deiratany.1@osu.edu>
2017-11-02 12:04:09 -0700

    angular 5 upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -10,7 +10,7 @@ import { PeopleModule } from './features/people/people.module';
 import { EventsModule } from './features/events/events.module';
 import { SpacesModule } from './features/spaces/spaces.module';
 import { routes } from './routes';
-import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome'
+// import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome'
 
 @NgModule({
   declarations: [
@@ -19,13 +19,12 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awes
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     StartupsModule,
     PeopleModule,
     EventsModule,
     SpacesModule,
-    RouterModule.forRoot(routes, { enableTracing: true }),
-    AngularFontAwesomeModule
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
